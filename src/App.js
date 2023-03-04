4a03ce7f71e12aca8b8ca4d881cce5fd0bc8c4bb
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-03-04 08:20:01 +0600

    editing added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import Header from './components/Header';
 import TaskList from './components/TaskList';
 
 export const deleteHandlerContext = createContext();
+export const EditHandlerContext = createContext();
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
+  const [editedText, setEditedText] = useState('');
+  const [toggleEditMode, setToggleEditMode] = useState(true);
 
   useEffect(() => {
     // getting data from server
@@ -32,7 +35,7 @@ const App = () => {
     }
   };
 
-  // delete event
+  // !delete event
   const handleDelete = (id) => {
     // delete data
     deleteData(id);
@@ -48,13 +51,41 @@ const App = () => {
     });
   };
 
+  // !edit event
+  const handleEdit = (id) => {
+    // set a target el for edit
+    const [editableTarget] = tasks.filter((task) => id === task.id);
+    editableTarget.isEditable = true;
+    setEditedText(editableTarget.text);
+    setTasks([...tasks]);
+    setToggleEditMode(false);
+    // re-arrange
+    tasks
+      .filter((task) => task.id === id)
+      .map((targetedEl) => (targetedEl.isEditable = false));
+    //  editing task form handler
+
+    //  set newly edited text
+  };
+  const handleEditSubmitter = (e, id) => {
+    console.log(id);
+  };
   return (
     <div className="wrapper bg-gradient-to-t from-gray-900 to-teal-900 min-h-screen text-xl text-gray-200 flex flex-col py-10 ">
       <deleteHandlerContext.Provider value={handleDelete}>
-        <Header />
-        <AddTask tasks={tasks} setTasks={setTasks} />
-        <TaskList tasks={tasks} error={error} loading={loading} />
-        <Footer />
+        <EditHandlerContext.Provider value={handleEdit}>
+          <Header />
+          <AddTask tasks={tasks} setTasks={setTasks} />
+          <TaskList
+            tasks={tasks}
+            error={error}
+            loading={loading}
+            handleEditSubmitter={handleEditSubmitter}
+            editedText={editedText}
+            setEditedText={setEditedText}
+          />
+          <Footer />
+        </EditHandlerContext.Provider>
       </deleteHandlerContext.Provider>
     </div>
   );
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,9 +1,10 @@
 import { useContext } from 'react';
 import { CiEdit } from 'react-icons/ci';
 import { MdDeleteOutline } from 'react-icons/md';
-import { deleteHandlerContext } from '../App';
-const TaskItem = ({ task }) => {
+import { deleteHandlerContext, EditHandlerContext } from '../App';
+const TaskItem = ({ task, handleEditSubmitter, editedText, setEditedText }) => {
   const handleDelete = useContext(deleteHandlerContext);
+  const handleEdit = useContext(EditHandlerContext);
 
   return (
     <div className="task-item flex justify-between items-center bg-gray-800 p-5 rounded hover:bg-gradient-to-r hover:from-teal-900 hover:to-gray-800 group">
@@ -11,14 +12,20 @@ const TaskItem = ({ task }) => {
         <span>
           <input type="checkbox" className="accent-teal-500" />
         </span>
-        <span>
+
+        {task.isEditable && (
+          <form>
+            <input required type="text" />
+          </form>
+        )}
+        {!task.isEditable && (
           <p className="group-hover:text-teal-500">{task.text}</p>
-        </span>
+        )}
       </div>
       <div className="task-item-right flex gap-3">
-        <span>
+        <button onClick={() => handleEdit(task.id)}>
           <CiEdit className="text-gray-500 hover:text-teal-500 cursor-pointer duration-300" />
-        </span>
+        </button>
         <button onClick={() => handleDelete(task.id)}>
           <MdDeleteOutline className="text-gray-500 hover:text-red-500 cursor-pointer duration-300" />
         </button>

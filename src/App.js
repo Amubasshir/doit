510303a1f16d1466ca6c9923ef2e4972394bb1b8
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-03-04 07:14:19 +0600

    delete added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import AddTask from './components/AddTask';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import TaskList from './components/TaskList';
 
+export const deleteHandlerContext = createContext();
+
 const App = () => {
   const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     // getting data from server
@@ -22,16 +26,36 @@ const App = () => {
       if (!res.ok) throw new Error('something went wrong');
       const data = await res.json();
       setTasks(data);
-    } catch (err) {
-      console.log(err.message);
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
     }
   };
+
+  // delete event
+  const handleDelete = (id) => {
+    // delete data
+    deleteData(id);
+    // set updated tasks
+    setTasks(tasks.filter((task) => id !== task.id));
+  };
+  const deleteData = async (id) => {
+    await fetch(`https://glorious-liberating-buckaroo.glitch.me/task/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+  };
+
   return (
     <div className="wrapper bg-gradient-to-t from-gray-900 to-teal-900 min-h-screen text-xl text-gray-200 flex flex-col py-10 ">
-      <Header />
-      <AddTask tasks={tasks} setTasks={setTasks} />
-      <TaskList tasks={tasks} />
-      <Footer />
+      <deleteHandlerContext.Provider value={handleDelete}>
+        <Header />
+        <AddTask tasks={tasks} setTasks={setTasks} />
+        <TaskList tasks={tasks} error={error} loading={loading} />
+        <Footer />
+      </deleteHandlerContext.Provider>
     </div>
   );
 };
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
 import { CiEdit } from 'react-icons/ci';
 import { MdDeleteOutline } from 'react-icons/md';
+import { deleteHandlerContext } from '../App';
 const TaskItem = ({ task }) => {
+  const handleDelete = useContext(deleteHandlerContext);
+
   return (
     <div className="task-item flex justify-between items-center bg-gray-800 p-5 rounded hover:bg-gradient-to-r hover:from-teal-900 hover:to-gray-800 group">
       <div className="task-item-left flex gap-2">
@@ -15,9 +19,9 @@ const TaskItem = ({ task }) => {
         <span>
           <CiEdit className="text-gray-500 hover:text-teal-500 cursor-pointer duration-300" />
         </span>
-        <span>
+        <button onClick={() => handleDelete(task.id)}>
           <MdDeleteOutline className="text-gray-500 hover:text-red-500 cursor-pointer duration-300" />
-        </span>
+        </button>
       </div>
     </div>
   );
